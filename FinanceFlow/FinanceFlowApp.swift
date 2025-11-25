fc342d0d26f5885161c75745084eb3bd71c296c0
Rafael Mukhametov <rafaelwww07@mail.ru>
2025-11-25 20:43:18 +0100

    Initial Commit

---
+++ FinanceFlow/FinanceFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceFlowApp.swift
+//  FinanceFlow
+//
+//  Created by Rafael Mukhametov on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinanceFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
